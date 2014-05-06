cca4b07fd7d94195c1e494e3c15ddee29de61d4b
damiengo <dam.gou@gmail.com>
2014-05-06 23:23:33 +0200

    mail

--- htdocs/api.php
+++ htdocs/api.php
@@ -8,30 +8,15 @@ use Symfony\Component\HttpFoundation\ParameterBag;
 /** App **/
 $app = new Silex\Application();
 
-/** Mail sender **/
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-
-/** Mailer options **/
-$app["swiftmailer.options"] = array(
-    "host"       => "smtp1.dc0.gpaas.net",
-    "port"       => "25",
-    "encryption" => null,
-    "auth_mode"  => null
-);
-
 /** Send response **/
 $app->post("/response", function(Request $request) use ($app) {
+    header("Cache-Control: max-age=1");
+
     $name  = $request->get("name", "");
     $email = $request->get("email", "");
     $text  = $request->get("message", "");
 
-    $message = \Swift_Message::newInstance()
-        ->setSubject("Reponse de [".$name."]")
-        ->setFrom(array($email))
-        ->setTo(array("dam.gou@gmail.com"))
-        ->setBody($text);
-
-    $app["mailer"]->send($message);
+    mail("dam.gou@gmail.com", "Reponse de [Nom: ".$name."][Email: ".$email."]", $text);
 
     return $app->json(["success" => true], 200);
 });
