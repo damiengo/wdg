aae372e643a36e7a1760565d4bf4c66f70b20e3b
damiengo <dam.gou@gmail.com>
2014-10-19 20:13:25 +0200

    Class

--- htdocs/photos/process.rb
+++ htdocs/photos/process.rb
@@ -6,26 +6,38 @@
 require 'fileutils'
 require 'mini_magick'
 
-# Paths
-img_path = 'assets/img/'
-mairie_path = img_path + 'mairie/'
+class ImageProcess
 
-original_dir = 'original/'
-medium_dir = 'medium/'
+  def initialize()
+    # Paths
+    @img_path     = 'assets/img/'
+    @original_dir = 'original/'
+    @medium_dir   = 'medium/'
+  end
 
-# Cleaning
-if File.directory?(mairie_path + medium_dir)
-  FileUtils.rm_r(mairie_path + medium_dir)
+  #
+  # Images generation.
+  #
+  def generate_images(name)
+    gen_path = @img_path + name + '/'
+    # Cleaning
+    if File.directory?(gen_path + @medium_dir)
+      FileUtils.rm_r(gen_path + @medium_dir)
+    end
+    Dir.mkdir(gen_path + @medium_dir)
+
+    Dir[gen_path + @original_dir + '*'].each do |img|
+      puts img
+      name = File.basename(img)
+      image = MiniMagick::Image.open(img)
+      image.resize "800"
+      image.format "jpg"
+      image.write gen_path + @medium_dir + name
+    end
+  end
+
 end
 
-Dir.mkdir(mairie_path + medium_dir)
-
-Dir[mairie_path + original_dir + '*'].each do |img|
-  puts img
-  name = File.basename(img)
-  image = MiniMagick::Image.open(img)
-  image.resize "800"
-  image.format "jpg"
-  image.write mairie_path + medium_dir + name
-end
-
+# Launching
+imgPrc = ImageProcess.new
+imgPrc.generate_images 'mairie'
